8a4ba1f333713a1793261ce88b3ab2ad93004f78
Laufer4u <nzevgolis4u@gmail.com>
2021-03-15 23:15:19 +0200

    Language

--- main.py
+++ main.py
@@ -1,36 +1,33 @@
-n=3
-L1=[
+n = 3
+L1 = [
     [],
     [3,1,2]
 ]
-for i in range(1,n+1):
-    L1[0].append(i)
-print (L1)
+for i in range(n):
+    L1[0].append(i+1)
+print(L1)
 L2=[
     [],
     [2,3,1]
 ]
-for i in range(1,n+1):
-    L2[0].append(i)
-print (L2)
-# L3 = L1 o L2 = id
-# L4 = L2 o L1 = id
-L3=[         
+for i in range(n):
+    L2[0].append(i+1)
+print(L2)
+L3=[     # L3 = L1 o L2 = id
     [],
     []
 ]
-
-for i in range(1,n+1):
-    L3[0].append(i)
-    j=L1[1][i-1]-1
-    L3[1].append(L2[1][j])
-print (L3)
-L4=[         
+for i in range(n):
+    L3[0].append(i+1)
+    j=L1[1][i]
+    L3[1].append(L2[1][j-1])
+print(L3)
+L4=[     # L4 = L2 o L1 = id
     [],
     []
 ]
-for i in range(1,n+1):
-    L4[0].append(i)
-    j=L2[1][i-1]-1
-    L4[1].append(L1[1][j])
-print (L4)
+for i in range(n):
+    L4[0].append(i+1)
+    j=L2[1][i]
+    L4[1].append(L1[1][j-1])
+print(L4)
